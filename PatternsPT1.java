75c64b2de00dc017b1186fdb593b3dfd3ad39f69
Harimati Ghoghari <84954383+iammoonchild@users.noreply.github.com>
2022-10-14 16:22:57 +0530

    Update PatternsPT1.java
    Hey, I have added one X pattern which is usually asked in interview rounds. Hope it'll be helpful.

--- PatternsPT1.java
+++ PatternsPT1.java
@@ -123,3 +123,94 @@ public class patt5 {
         }
     }
 }
+
+
+
+/*X Pattern 
+
+*     *
+
+ *   *
+
+  * *
+
+   *
+
+  * *
+
+ *   *
+
+*     *  */
+
+package Patterns;
+
+import java.util.*;
+
+public class Star {
+
+public static void main(String args[]) {
+
+int i, j, n;
+
+Scanner sc = new Scanner(System.in);
+
+System.out.println("Enter a value for n");
+
+n = sc.nextInt(); //Input
+
+//Upper V pattern
+
+for (i = n; i >= 1; i--) {
+
+for (j = i; j < n; j++) {
+
+System.out.print(" ");//print spaces
+
+}
+
+for (j = 1; j <= (2 * i - 1); j++) {
+
+if (j == 1 || j == (2 * i - 1))//Logic for printing star
+
+System.out.print("*");
+
+else
+
+System.out.print(" ");//if logic fails print space
+
+}
+
+System.out.println("");
+
+}
+
+//Lower Inverted V pattern
+
+for (i = 2; i <= n; i++) {
+
+for (j = i; j < n; j++) {
+
+System.out.print(" ");//Print spaces
+
+}
+
+for (j = 1; j <= (2 * i - 1); j++) {
+
+if (j == 1 || j == (2 * i - 1))//Logic for printing star
+
+System.out.print("*");
+
+else
+
+System.out.print(" ");//if logic fails print space
+
+}
+
+System.out.println("");
+
+}
+
+}
+
+}
+
